5eeda05d17783bc66f1feee3c16ef41bc4810efd
Arabel Albrecht <bebelalbrecht@gmail.com>
2026-03-23 18:53:24 -0300

    estatisticas histograma grafico

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ import streamlit as st
 st.header('Dashboard de Anúncios de Carros 🚗')
 
 car_data = pd.read_csv('vehicles.csv')  # arquivo na raiz do projeto
+
+if st.checkbox('Mostrar estatísticas descritivas'):
+    st.write(car_data.describe())
+    
+if st.button('Criar histograma'):
+    st.write('Histograma da quilometragem')
+    fig_hist = px.histogram(car_data, x='odometer', nbins=50)
+    st.plotly_chart(fig_hist, use_container_width=True)
+    
